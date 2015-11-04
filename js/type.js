0e3c57db2050b87f62e2539880f53e54feecc2da
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-11-04 15:11:18 -0800

    #9 added jquery functionality for typed.js

---
+++ js/type.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(".type").typed({
+    strings: ["developer", "builder", "foodie", "coffee lover", "pug fanatic"],
+    typeSpeed: 0,
+    loop: true,
+    showCursor: true,
+    cursorChar: "|"
+  });
+})
