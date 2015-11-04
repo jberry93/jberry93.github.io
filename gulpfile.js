60a47f1123c91356883cb2e1f74e6fb5b695efea
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-11-04 15:10:36 -0800

    Added 2 gulp tasks to compress all vendor js into one file and to compress images

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require("gulp"),
     browserSync = require("browser-sync").create(),
+    imageMin = require("gulp-imagemin"),
     nodemon = require("gulp-nodemon"),
     mocha = require("gulp-mocha"),
     concat = require("gulp-concat"),
@@ -24,4 +25,16 @@ gulp.task("smashCSS", function() {
     "bower_components/animate.css/animate.min.css",
     "bower_components/bootstrap/dist/css/bootstrap.min.css"
   ]).pipe(concat("all.css")).pipe(gulp.dest("server/public"));
+});
+
+gulp.task("smashJS", function() {
+  return gulp.src([
+    "bower_components/jquery/dist/jquery.min.js",
+    "bower_components/typed.js/dist/typed.min.js",
+    "bower_components/bootstrap/dist/js/bootstrap.min.js"
+  ]).pipe(concat("vendor.js")).pipe(uglify()).pipe(gulp.dest("server/public"));
+});
+
+gulp.task("compressImg", function() {
+  return gulp.src("images/*").pipe(imageMin()).pipe(gulp.dest("server/public"));
 });
