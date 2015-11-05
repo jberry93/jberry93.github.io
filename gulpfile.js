84d36dba9ac3e30301adedae9f3f2b1b4e1b3cae
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-11-04 17:06:00 -0800

    Updated file structure again

--- gulpfile.js
+++ gulpfile.js
@@ -12,42 +12,36 @@ var gulp = require("gulp"),
 gulp.task("browserSync", function() {
   browserSync.init({
     server: {
-      baseDir: "./client/views"
+      baseDir: "./"
     }
   });
-  gulp.watch("client/views/*", browserSync.reload);
+  gulp.watch("index.html", browserSync.reload);
+  gulp.watch("default.css", browserSync.reload);
   gulp.watch("js/*.js", browserSync.reload);
-});
-
-gulp.task("minHTML", function() {
-  return gulp.src("client/views/index.html")
-    .pipe(minHTML())
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(gulp.dest("server/public"));
 });
 
 gulp.task("smashCSS", function() {
   return gulp.src([
-    "client/bower_components/animate.css/animate.min.css",
-    "client/bower_components/bootstrap/dist/css/bootstrap.min.css"
-  ]).pipe(concat("all.min.css")).pipe(minCSS()).pipe(gulp.dest("client/views"));
+    "bower_components/animate.css/animate.min.css",
+    "bower_components/bootstrap/dist/css/bootstrap.min.css"
+  ]).pipe(concat("all.min.css")).pipe(minCSS()).pipe(gulp.dest("./"));
 });
 
 gulp.task("uglify", function() {
   return gulp.src("client/js/*.js")
     .pipe(concat("my.min.js"))
     .pipe(uglify())
-    .pipe(gulp.dest("client/views"));
+    .pipe(gulp.dest("./"));
 });
 
 gulp.task("smashVendorJS", function() {
   return gulp.src([
-    "client/bower_components/jquery/dist/jquery.min.js",
-    "client/bower_components/typed.js/dist/typed.min.js",
-    "client/bower_components/bootstrap/dist/js/bootstrap.min.js"
-  ]).pipe(concat("vendor.min.js")).pipe(uglify()).pipe(gulp.dest("client/views"));
+    "bower_components/jquery/dist/jquery.min.js",
+    "bower_components/typed.js/dist/typed.min.js",
+    "bower_components/bootstrap/dist/js/bootstrap.min.js"
+  ]).pipe(concat("vendor.min.js")).pipe(uglify()).pipe(gulp.dest("./"));
 });
 
 gulp.task("compressImg", function() {
-  return gulp.src("client/images/*").pipe(imageMin()).pipe(gulp.dest("client/views/minImage"));
+  return gulp.src("images/*").pipe(imageMin()).pipe(gulp.dest("minImage"));
 });
