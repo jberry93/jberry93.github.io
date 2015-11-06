ead845616fdb53e0ba738bc52196251edf234cca
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-11-06 13:07:09 -0800

    Added semantic-ui and made navigation mobile-friendly

--- client/js/type.js
+++ client/js/type.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+  $(".masthead").visibility({
+    once: false,
+    onBottomPassed: function() {
+      $(".fixed.menu").transition("fade in");
+    },
+    onBottomPassedReverse: function() {
+      $(".fixed.menu").transition("fade out");
+    }
+  });
+  $(".ui.sidebar").sidebar("attach events", ".toc.item");
   $(".type").typed({
     strings: ["developer", "builder", "foodie", "coffee lover", "pug fanatic"],
     typeSpeed: 0,
--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,22 @@ gulp.task("browserSync", function() {
 gulp.task("smashCSS", function() {
   return gulp.src([
     "bower_components/animate.css/animate.min.css",
-    "bower_components/bootstrap/dist/css/bootstrap.min.css"
+    "bower_components/semantic/dist/semantic.min.css",
+    "bower_components/semantic/dist/components/reset.min.css",
+    "bower_components/semantic/dist/components/site.min.css",
+    "bower_components/semantic/dist/components/container.min.css",
+    "bower_components/semantic/dist/components/grid.min.css",
+    "bower_components/semantic/dist/components/header.min.css",
+    "bower_components/semantic/dist/components/image.min.css",
+    "bower_components/semantic/dist/components/menu.min.css",
+    "bower_components/semantic/dist/components/divider.min.css",
+    "bower_components/semantic/dist/components/dropdown.min.css",
+    "bower_components/semantic/dist/components/segment.min.css",
+    "bower_components/semantic/dist/components/button.min.css",
+    "bower_components/semantic/dist/components/list.min.css",
+    "bower_components/semantic/dist/components/icon.min.css",
+    "bower_components/semantic/dist/components/sidebar.min.css",
+    "bower_components/semantic/dist/components/transition.min.css"
   ]).pipe(concat("all.min.css")).pipe(minCSS()).pipe(gulp.dest("./"));
 });
 
@@ -37,8 +52,11 @@ gulp.task("uglify", function() {
 gulp.task("smashVendorJS", function() {
   return gulp.src([
     "bower_components/jquery/dist/jquery.min.js",
-    "bower_components/typed.js/dist/typed.min.js",
-    "bower_components/bootstrap/dist/js/bootstrap.min.js"
+    "bower_components/semantic/dist/semantic.min.js",
+    "bower_components/semantic/dist/components/visibility.min.js",
+    "bower_components/semantic/dist/components/sidebar.min.js",
+    "bower_components/semantic/dist/components/transition.min.js",
+    "bower_components/typed.js/dist/typed.min.js"
   ]).pipe(concat("vendor.min.js")).pipe(uglify()).pipe(gulp.dest("./"));
 });
 
