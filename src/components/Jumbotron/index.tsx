d67cea0d961cde5a4c00d19a5279c88db78640f0
jberry93 <jeffreyberry93@gmail.com>
2019-01-04 14:59:48 -0800

    refactor(components): flesh out some components

--- src/components/Jumbotron/index.tsx
+++ src/components/Jumbotron/index.tsx
@@ -3,6 +3,20 @@ import './Jumbotron.scss';
 
 export const Jumbotron: React.SFC<{}> = () => (
     <div className="jumbotron">
-        <h2>Testing</h2>
+        <div className="centerize">
+            <h2>Jeffrey Berry</h2>
+            <div className="social-media">
+                <a
+                    className="btn"
+                    href="https://www.linkedin.com/in/jeffrey-berry-82154a3a/"
+                    target="_blank"
+                >LinkedIn</a>
+                <a
+                    className="btn"
+                    href="https://github.com/jberry93"
+                    target="_blank"
+                >Github</a>
+            </div>
+        </div>
     </div>
 );
